596edd497fb0c5b391574c4af8660d5815c31b72
Romain VORLET <romain.vorlet@gmail.com>
2024-01-11 23:47:59 +0100

    implémentation retrofit API parcelle et geomtutation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
         resources.excludes.add("META-INF/LICENSE.md")
         resources.excludes.add("META-INF/LICENSE-notice.md")
     }
+
+
 }
 
 dependencies {
@@ -52,6 +54,7 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
