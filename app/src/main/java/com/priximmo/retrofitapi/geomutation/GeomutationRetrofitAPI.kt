133f359b384a4a6ef7869c51851df75c2f8a5de6
Romain VORLET <romain.vorlet@gmail.com>
2024-01-16 23:42:53 +0100

    remplacement object GEomutationRetrofitAPI

--- app/src/main/java/com/priximmo/retrofitapi/geomutation/GeomutationRetrofitAPI.kt
+++ app/src/main/java/com/priximmo/retrofitapi/geomutation/GeomutationRetrofitAPI.kt
@@ -1,4 +1,28 @@
 package com.priximmo.retrofitapi.geomutation
 
-object GeomutationService {
+import com.priximmo.servicepublicapi.RequestInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.jackson.JacksonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object GeomutationRetrofitAPI {
+
+    private const val BASE_URL = "https://apidf-preprod.cerema.fr/dvf_opendata/"
+
+    val okHttpClient = OkHttpClient()
+        .newBuilder()
+        .addInterceptor(RequestInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    fun getClient() : Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 }
