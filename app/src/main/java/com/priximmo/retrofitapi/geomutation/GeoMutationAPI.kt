db9be53e283be31efb7e217b51459e3386d5441c
Romain VORLET <romain.vorlet@gmail.com>
2024-02-11 15:53:11 +0100

    ajout tag pour chips

--- app/src/main/java/com/priximmo/retrofitapi/geomutation/GeoMutationAPI.kt
+++ app/src/main/java/com/priximmo/retrofitapi/geomutation/GeoMutationAPI.kt
@@ -7,5 +7,8 @@ import retrofit2.http.Query
 
 interface GeoMutationAPI {
     @GET("geomutations/?")
-    fun searchGeomutation(@Query("anneemut_min") anneeMutMin:String, @Query("in_bbox") bbox:String, @Query("code_insee") codeInsee:String): Call<Geomutation>
+    fun searchGeomutationByAnneeMin(@Query("anneemut_min") anneeMutMin:String, @Query("in_bbox") bbox:String, @Query("code_insee") codeInsee:String): Call<Geomutation>
+
+    @GET("geomutations/")
+    fun searchGeomutationForSingleYear(@Query("anneemut") anneeMin:String, @Query("in_bbox") bbox:String, @Query("code_insee") codeInsee:String): Call<Geomutation>
 }
